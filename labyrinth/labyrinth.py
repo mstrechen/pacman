fa44ae952f18eeae6f8325736dc1e5c5efb23931
StasTomash <stastomash1@gmail.com>
2021-02-12 04:31:04 +0200

    Add file parser

---
+++ labyrinth/labyrinth.py
@@ -0,0 +1,37 @@
+from typing import Set, Tuple, List
+
+
+class Labyrinth:
+    def __init__(self):
+        self.cells: Set[Tuple[int, int]] = set()
+        self.edges: dict[Tuple[int, int], Set[Tuple[int, int]]] = dict()
+        self.raw_img: List[str] = []
+
+    def _add_edge(self, cell_from: Tuple[int, int], cell_to: Tuple[int, int]) -> None:
+        if self.edges.get(cell_from) is None:
+            self.edges[cell_from] = set()
+        self.edges[cell_from].add(cell_to)
+
+    def load_from_file(self, filepath: str) -> None:
+        with open(filepath) as input_file:
+            line_num = 0
+            prev_line = ''
+            for line in input_file:
+                if line == "":
+                    break
+                self.raw_img.append(line)
+                for pos in range(len(line) - 1):
+                    if line[pos] == ' ':
+                        self.cells.add((line_num, pos))
+                        if line_num != 0 and prev_line[pos] == ' ':
+                            self._add_edge((line_num, pos), (line_num - 1, pos))
+                            self._add_edge((line_num - 1, pos), (line_num, pos))
+                        if pos != 0 and line[pos - 1] == ' ':
+                            self._add_edge((line_num, pos), (line_num, pos - 1))
+                            self._add_edge((line_num, pos - 1), (line_num, pos))
+                        if pos == 0 and line[-2] == ' ':
+                            self._add_edge((line_num, 0), (line_num, len(line) - 2))
+                            self._add_edge((line_num, len(line) - 2), (line_num, 0))
+
+                line_num += 1
+                prev_line = line
---
+++ tests/test_labyrinth.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+from labyrinth.labyrinth import Labyrinth
+
+
+class TestLabyrinth(TestCase):
+    def test_load_from_file(self):
+        raw_data = ["######\n",
+                    "  ##  \n",
+                    "#    #\n",
+                    "# ## #\n",
+                    "#    #\n",
+                    "######\n"]
+        with open("test_file.txt", "w+") as test_file:
+            test_file.writelines(raw_data)
+        labyrinth = Labyrinth()
+        labyrinth.load_from_file("test_file.txt")
+
+        expected_cells = {
+            (1, 0), (1, 1), (1, 4), (1, 5),
+            (2, 1), (2, 2), (2, 3), (2, 4),
+            (3, 1), (3, 4),
+            (4, 1), (4, 2), (4, 3), (4, 4)
+        }
+
+        expected_edges = {
+            (1, 0): {(1, 1), (1, 5)},
+            (1, 1): {(1, 0), (2, 1)},
+            (1, 4): {(1, 5), (2, 4)},
+            (1, 5): {(1, 4), (1, 0)},
+            (2, 1): {(1, 1), (3, 1), (2, 2)},
+            (2, 2): {(2, 1), (2, 3)},
+            (2, 3): {(2, 2), (2, 4)},
+            (2, 4): {(1, 4), (2, 3), (3, 4)},
+            (3, 1): {(2, 1), (4, 1)},
+            (3, 4): {(2, 4), (4, 4)},
+            (4, 1): {(3, 1), (4, 2)},
+            (4, 2): {(4, 1), (4, 3)},
+            (4, 3): {(4, 2), (4, 4)},
+            (4, 4): {(4, 3), (3, 4)},
+        }
+
+        self.assertEqual(expected_cells, labyrinth.cells)
+        self.assertEqual(expected_edges, labyrinth.edges)
+        self.assertEqual(raw_data, labyrinth.raw_img)
